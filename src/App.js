458298b9a4b5e426d5846bb98b0f3a60d2dd1973
Kyler Smith <kysmith327@gmail.com>
2020-10-17 08:26:16 -0700

    Native Start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
+import {VectorMap} from 'react-jvectormap';
+import PropTypes from 'prop-types';
+import $ from 'jquery-mousewheel';
 
 function App() {
+
+var mapData = {
+  AU: 760,
+  BR: 550,
+  CA: 120,
+  DE: 1300,
+  FR: 540,
+  GB: 690,
+  GE: 200,
+  IN: 200,
+  RO: 600,
+  RU: 300,
+  US: 2920
+};
+
+const regionStyle = {
+  initial: {
+    fill: "#e4e4e4",
+    "fill-opacity": 0.9,
+    stroke: "none",
+    "stroke-width": 0,
+    "stroke-opacity": 0
+  }
+};
+
+const series = {
+  regions: [
+    {
+      values: mapData,
+      scale: ["#AAAAAA", "#444444"],
+      normalizeFunction: "polynomial"
+    }
+  ]
+}
+
+const containerStyle = {
+  width: "100%",
+  height: "100%"
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="jvectormap-custom">
+      
+        
+      <VectorMap
+      map={"world_mill"}
+      backgroundColor="transparent"
+      zoomOnScroll={false}
+      containerStyle={containerStyle}
+      containerClassName="map"
+      regionStyle={regionStyle}
+      series={series}
+    />
+      
     </div>
   );
 }
